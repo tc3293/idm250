9f24164b12b281c8ba6d73aaf476178bb4e5e940
Tony Chen <91432463+tc3293@users.noreply.github.com>
2023-02-20 19:25:57 -0500

    update

--- wp-content/themes/IDM250-theme/footer.php
+++ wp-content/themes/IDM250-theme/footer.php
@@ -1,8 +1,21 @@
+<br><br><br><br><br><br>
 <footer>
-  <p>&copy; <?php echo date('Y'); ?>
-  <?php bloginfo('name'); ?></p>
+  <div id="footer">
+    <div class="footer2">
 <?php wp_nav_menu(['theme_location' => 'footer-menu']); ?>
-</footer>
+<div class ="icons">
+  <p>Coming soon, Social Media icon here</p>
+</div></div>
+
+
+<br>
+<p class="copyright"> Copyright&copy; <?php echo date('Y'); ?> 
+<?php bloginfo('name'); ?> - All Right reserved By Tony Chen</p>
+<br></div>
+
+
+
+  </footer>
 <?php // https://developer.wordpress.org/reference/functions/wp_footer/?>
 <?php wp_footer(); ?>
 </body>
